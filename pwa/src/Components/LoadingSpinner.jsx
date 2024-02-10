3aadd625ae8dabf688f09f3d9a1c882a58a8fc73
JCCode288 <jendycaprico@gmail.com>
2024-02-11 03:17:27 +0700

    feat: add base main logo feat: add base spinner feat: integrate splash screen

--- pwa/src/Components/LoadingSpinner.jsx
+++ pwa/src/Components/LoadingSpinner.jsx
@@ -1,5 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 
-export default function LoadingSpinner() {
-  return <Box>Loading nih....</Box>;
+export default function LoadingSpinner({ size = "sm" }) {
+  return (
+    <Spinner
+      size={size}
+      thickness="0.3rem"
+      speed="0.75s"
+      emptyColor="green.200"
+      color="green.500"
+    />
+  );
 }
--- pwa/src/Components/Navbar.jsx
+++ pwa/src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import MainLogo from "./MainLogo";
 
 export default function Navbar() {
   return (
-    <Box justifyContent="start" display="flex" alignSelf="start">
+    <Box justifyContent="start" display="flex" alignSelf="start" px="1rem">
       <Link to="/">
-        <Box whiteSpace="pre-line">Home Logo</Box>
+        <MainLogo size="48" />
       </Link>
     </Box>
   );
